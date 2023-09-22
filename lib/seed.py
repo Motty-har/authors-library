8145cd20548f7da70665fec0d9e70d2d61e11509
Motty Harlig <harligmotty@gmail.com>
2023-09-22 01:21:51 -0400

    initial commit

---
+++ lib/seed.py
@@ -0,0 +1,8 @@
+from models.__init__ import CONN, CURSOR
+#from models.dog import Dog
+def seed_database():
+    #drop tables and create tables 
+    pass
+
+seed_database()
+print("seeded database")
