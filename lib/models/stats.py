99913a2d93e35322f6d4270ce4e67bab2c40bdbf
Motty Harlig <harligmotty@gmail.com>
2023-10-02 15:50:49 -0400

    checked if goals and assists are numbers

--- lib/models/stats.py
+++ lib/models/stats.py
@@ -4,7 +4,7 @@ from models.players import Player
 class Stats:
     all = {}
 
-    def __init__(self, goals, assists, points, player_id, id=None):
+    def __init__(self, goals, assists, player_id, id=None):
         self.id = id
         self.goals = goals
         self.assists = assists
@@ -13,6 +13,32 @@ class Stats:
 
     def __repr__(self):
         return (
-            f"<Employee {self.id}: {self.name}, {self.job_title}, " +
-            f"Department ID: {self.department_id}>"
+            f"<Stats {self.id}, {self.goals}: {self.assists}, {self.points}, " +
+            f"Player ID: {self.player_id}>"
         )
+    
+    @property
+    def goals(self):
+        return self._goals
+
+    @goals.setter
+    def goals(self, goals):
+        if isinstance(goals, int):
+            self._goals = goals
+        else:
+            raise ValueError(
+                "Goals must be a number"
+            )
+
+    @property
+    def assists(self):
+        return self._assists
+
+    @assists.setter
+    def assists(self, assists):
+        if isinstance(assists, int):
+            self._assists = assists
+        else:
+            raise ValueError(
+                "Assist must be a number"
+            )
